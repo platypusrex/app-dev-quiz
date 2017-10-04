5b239cbf3ddc97304428cdd237bdc728e8ced4f2
platypusrex <frankcooke79@gmail.com>
2017-10-04 12:11:36 -0400

    Rework - add enums for game type, game status, game category, and player to the game model

--- src/shared/models/game.model.ts
+++ src/shared/models/game.model.ts
@@ -1,20 +1,48 @@
+export type IGameType =
+  'java' |
+  'cSharp' |
+  'c' |
+  'cPlusPlus' |
+  'javascript' |
+  'python' |
+  'php' |
+  'elixer' |
+  'objectiveC' |
+  'ruby' |
+  'go' |
+  'sql' |
+  'scala' |
+  'elm' |
+  'haskell' |
+  'perl' |
+  'erlang' |
+  'clojure' |
+  'css' |
+  'html';
+
+export type IGameCategory = 'onePlayer' | 'twoPlayer'
+export type IPlayerStatus = 'joined' | 'cancelled';
+export type IGameStatus = 'pending' | 'started' | 'cancelled' | 'timedOut' | 'finished';
+
 export interface IGame {
   _id?: string;
   createdOn?: Date;
   room?: string;
-  status?: 'started' | 'pending';
-  type?: string;
+  status?: IGameStatus;
+  type?: IGameType;
+  category?: IGameCategory;
   players?: IPlayer[];
 }
 
 export interface ICreateGame {
   room?: string;
-  type?: string;
+  type?: IGameType;
+  category?: IGameCategory;
   userName?: string;
 }
 
 interface IPlayer {
   _id: string;
   userName?: string;
-  status?: 'joined';
+  status?: IPlayerStatus;
 }

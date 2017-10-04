34823bb4fb1ca72d16aab8353a30cccd9cad37b6
platypusrex <frankcooke79@gmail.com>
2017-10-04 12:09:49 -0400

    Refactor - logic in game-socket service, adds handler for creating/leaving one player game and checking for both player joined

--- src/services/game-socket.service.ts
+++ src/services/game-socket.service.ts
@@ -33,38 +33,102 @@ export class GameSocketService {
       this.store.dispatch(this.gamesActions.updateGameMessages(msgData));
     });
 
-    this.socket.on(gameEvents.gameCreated, (gameData: IGame) => {
+    this.getOnePlayerGameEventListeners();
+    this.getTwoPlayerGameEventListeners();
+    this.getGameEventListeners();
+  }
+
+  handleCreateOnePlayerGame(createGameData: ICreateGame) {
+    this.socket.emit(gameEvents.createOnePlayerGame, createGameData);
+  }
+
+  handleCreateTwoPlayerGame(createGameData: ICreateGame) {
+    this.socket.emit(gameEvents.createTwoPlayerGame, createGameData);
+  }
+
+  handleCheckForBothPlayersJoined (userName: string) {
+    if (this.game) {
+      const playersJoined = this.game.players.filter(player => player.status === 'joined');
+      if (playersJoined.length !== 2) {
+        this.handleLeaveTwoPlayerGame(userName, 'timedOut');
+        this.store.dispatch(this.loadingActions.loadingFinish());
+        return true;
+      }
+      return false;
+    }
+  }
+
+  handleLeaveOnePlayerGame(userName: string) {
+    if (this.game) {
+      this.socket.emit(gameEvents.cancelOnePlayerGame, {
+        room: this.game.room,
+        id: this.game._id,
+        status: 'cancelled',
+        userName: userName
+      });
+    }
+    this.socket.emit(gameEvents.leaveRoom);
+    this.store.dispatch(this.gamesActions.clearGameData());
+  }
+
+  handleLeaveTwoPlayerGame(userName?: string, gameStatus?: string) {
+    if (userName) {
+      this.socket.emit(gameEvents.cancelTwoPlayerGame, {
+        room: this.game.room,
+        id: this.game._id,
+        status: gameStatus || 'cancelled',
+        userName: userName
+      });
+    }
+
+    if (!userName) {
+      this.socket.emit(gameEvents.leaveRoom);
+    }
+
+    this.store.dispatch(this.gamesActions.clearGameData());
+  }
+
+  handleGetNewQuestion(category: string) {
+    this.socket.emit(gameEvents.newQuestion, category);
+  }
+
+  getGameEventListeners() {
+    this.socket.on(gameEvents.newQuestionSuccess, (questionData: any) => {
+      console.log(questionData);
+      // TODO: update trivia questions store
+    });
+  }
+
+  getOnePlayerGameEventListeners() {
+    this.socket.on(gameEvents.onePlayerGameCreated, (gameData: IGame) => {
       this.store.dispatch(this.gamesActions.updateGameData(gameData));
-      this.store.dispatch(this.loadingActions.loadingStart());
-      this.socket.emit(gameEvents.gameCreatedSuccess, this.game);
+      this.socket.emit(gameEvents.onePlayerGameCreatedSuccess, this.game);
     });
 
-    this.socket.on(gameEvents.gameStarted, (gameData: IGame) => {
-      this.store.dispatch(this.gamesActions.updateGameData(gameData));
-      this.store.dispatch(this.loadingActions.loadingFinish());
-      this.socket.emit(gameEvents.gameStartedSuccess, this.game);
-    });
-
-    this.socket.on(gameEvents.gameCanceled, (msgData: IChat) => {
+    this.socket.on(gameEvents.onePlayerGameCanceled, (msgData: IChat) => {
       if (this.game) {
         this.store.dispatch(this.gamesActions.updateGameMessages(msgData));
       }
     });
   }
 
-  handleCreateTwoPlayerGame(createGameData: ICreateGame) {
-    this.socket.emit(gameEvents.createTwoPlayerGame, createGameData)
-  }
+  getTwoPlayerGameEventListeners() {
+    this.socket.on(gameEvents.twoPlayerGameCreated, (gameData: IGame) => {
+      this.store.dispatch(this.gamesActions.updateGameData(gameData));
+      this.store.dispatch(this.loadingActions.loadingStart());
+      this.socket.emit(gameEvents.twoPlayerGameCreatedSuccess, this.game);
+    });
 
-  handleCancelGameInProgress(userName: string) {
-    if (this.game) {
-      this.socket.emit(gameEvents.cancelTwoPlayerGame, {
-        room: this.game.room,
-        id: this.game._id,
-        userName: userName
-      });
-      this.socket.emit(gameEvents.disconnect);
-    }
-    this.store.dispatch(this.gamesActions.clearGameData());
+    this.socket.on(gameEvents.twoPlayerGameStarted, (gameData: IGame) => {
+      this.store.dispatch(this.gamesActions.updateGameData(gameData));
+      this.store.dispatch(this.loadingActions.loadingFinish());
+      this.socket.emit(gameEvents.twoPlayerGameStartedSuccess, this.game);
+    });
+
+    this.socket.on(gameEvents.twoPlayerGameCanceled, (msgData: IChat) => {
+      if (this.game) {
+        this.store.dispatch(this.gamesActions.updateGameMessages(msgData));
+      }
+    });
   }
 }

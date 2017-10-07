76382312bd47bf684dc202c73a92d88d5cbfb2c5
platypusrex <frankcooke79@gmail.com>
2017-10-07 16:25:02 -0400

    Fix - import for games.actions in games.effects

--- src/state/effects/games.effects.ts
+++ src/state/effects/games.effects.ts
@@ -3,7 +3,7 @@ import { Action, Store } from '@ngrx/store';
 import { Effect, Actions } from '@ngrx/effects';
 import { AppState } from '../app.state';
 import { GamesService } from '../../services/games.service';
-import { GamesActions } from '../../state/actions/game.actions';
+import { GamesActions } from '../actions/game.actions';
 import 'rxjs/add/operator/mergeMap';
 import { Observable } from "rxjs";
 
@@ -13,7 +13,7 @@ export class GamesEffects {
     private store: Store<AppState>,
     private actions$: Actions,
     private gamesService: GamesService,
-    private gamesActions: GamesActions
+    private gamesActions: GamesActions,
   ) {}
 
   @Effect() getGameCategories: Observable<Action> = this.actions$
@@ -21,5 +21,5 @@ export class GamesEffects {
     .mergeMap(action => this.gamesService.getGameCategories()
       .map(categories => this.gamesActions.getCategoriesSuccess(categories))
       .catch(err => Observable.of(this.gamesActions.getCategoriesFailure(err)))
-    )
+    );
 }

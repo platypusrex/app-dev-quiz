43b0112e9b97706e7d02fcc2bf816c795a1bb454
platypusrex <frankcooke79@gmail.com>
2017-10-07 16:23:20 -0400

    Add - GetTriviaQuestion to triviaQuestion model

--- src/shared/models/trivia-question.model.ts
+++ src/shared/models/trivia-question.model.ts
@@ -1,7 +1,13 @@
-interface ITriviaQuestion {
-  _id: string;
+export interface ITriviaQuestion {
+  _id?: string;
   question?: string;
   answer?: string;
   category?: string;
   choices?: string[];
+}
+
+export interface IGetTriviaQuestion {
+  category: string;
+  room?: string;
+  id: string | null;
 }

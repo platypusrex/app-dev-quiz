4b342fe03f677691140ed812322d1da2f2ed10f3
platypusrex <frankcooke79@gmail.com>
2017-10-13 14:22:50 -0400

    Remove - console.log from trivia-question.actions

--- src/state/actions/trivia-question.actions.ts
+++ src/state/actions/trivia-question.actions.ts
@@ -6,7 +6,6 @@ import { ITriviaQuestion } from '../../shared/models/trivia-question.model';
 export class TriviaQuestionActions {
   static UPDATE_TRIVIA_QUESTION = 'UPDATE_TRIVIA_QUESTION';
   updateTriviaQuestion(triviaQuestion: ITriviaQuestion): Action {
-    console.log('action', triviaQuestion);
     return {
       type: TriviaQuestionActions.UPDATE_TRIVIA_QUESTION,
       payload: triviaQuestion
@@ -15,7 +14,6 @@ export class TriviaQuestionActions {
 
   static CLEAR_TRIVIA_QUESTION = 'CLEAR_TRIVIA_QUESTION';
   clearTriviaQuestion(): Action {
-    console.log('clear action');
     return {
       type: TriviaQuestionActions.CLEAR_TRIVIA_QUESTION
     }

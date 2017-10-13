9eef3495279574b6dfe4ff693f64b63f7de95207
platypusrex <frankcooke79@gmail.com>
2017-10-13 14:13:39 -0400

    Add - game check answer component template, scss partial, and controller ( this probably needs to renamed )

---
+++ src/components/game/game-check-answer/game-check-answer.component.ts
@@ -0,0 +1,61 @@
+import { Component, Input, OnChanges, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { animations } from '../../../shared/animations/index';
+import { bounceInDown, bounceInOut } from '../../../shared/animations/animations';
+
+@Component({
+  selector: 'game-check-answer-cmp',
+  templateUrl: 'game-check-answer.component.html',
+  animations: [
+    animations(550, 100, 'ease-in-out'),
+    bounceInOut,
+    bounceInDown
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GameCheckAnswerComponent implements OnChanges {
+  @Input() showMessage: boolean;
+  @Input() isCorrect: boolean;
+  correctMessages: string[] = [
+    'Great job!',
+    'Nice work!',
+    'Perfect!',
+    'Awesome work!',
+    'Way to go!'
+  ];
+  incorrectMessages: string[] = [
+    'Sorry dude.',
+    'Err...no.',
+    `Nope.`,
+    'Incorrect.',
+    'Maybe next time?'
+  ];
+  animationsTypes: string[] = [
+    'fadeIn',
+    'rotateIn',
+    'zoomIn',
+    'slideIn',
+    'bounceIn'
+  ];
+  animationDirections: string[] = [
+    'Down',
+    'Left',
+    'Right',
+  ];
+  animation: string;
+  message: string;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngOnChanges(thing) {
+    console.log(thing);
+    this.animation = this.animationsTypes[this.getRandomNum(0,5)] + this.animationDirections[this.getRandomNum(0,3)];
+    this.message = this.isCorrect ?
+      this.correctMessages[this.getRandomNum(0, 5)] :
+      this.incorrectMessages[this.getRandomNum(0, 5)];
+    this.cdr.markForCheck();
+  }
+
+  getRandomNum(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min));
+  }
+}
---
+++ src/components/game/game-check-answer/index.ts
@@ -0,0 +1,1 @@
+export * from './game-check-answer.component';

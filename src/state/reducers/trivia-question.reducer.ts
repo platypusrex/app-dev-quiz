b96d58d7bd5bb6ef0333b3fb080a0befa2a1519b
platypusrex <frankcooke79@gmail.com>
2017-10-21 18:11:19 -0400

    Rework - updates trivia-question.reducer state from single question object to an array of question objects

--- src/state/reducers/trivia-question.reducer.ts
+++ src/state/reducers/trivia-question.reducer.ts
@@ -3,17 +3,19 @@ import { TriviaQuestionActions } from '../actions/trivia-question.actions';
 import { ITriviaQuestion} from '../../shared/models/trivia-question.model';
 
 export interface ITriviaQuestionState {
-  triviaQuestion: ITriviaQuestion | null;
+  triviaQuestions: ITriviaQuestion[] | null;
+  error: string | null;
 }
 
 const initialState: ITriviaQuestionState = {
-  triviaQuestion: null
+  triviaQuestions: null,
+  error: null
 };
 
 export const TriviaQuestionReducer: ActionReducer<any> = (state: ITriviaQuestionState = initialState, action: Action) => {
   switch(action.type) {
-    case TriviaQuestionActions.UPDATE_TRIVIA_QUESTION:
-      return {...state, triviaQuestion: {...state.triviaQuestion, ...action.payload}};
+    case TriviaQuestionActions.GET_TRIVIA_QUESTION_COLLECTION:
+      return {...state, triviaQuestions: {...state.triviaQuestions, ...action.payload}};
 
     case TriviaQuestionActions.CLEAR_TRIVIA_QUESTION:
       return initialState;
